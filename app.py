07274036071ad32944c14080b5394f49c6f846a5
Avantikampatil <avantikapatil008@gmail.com>
2025-05-07 08:09:20 +0530

    fix the port and commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from googletrans import Translator
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -34,5 +35,6 @@ def listen_message():
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
